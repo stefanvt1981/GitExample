d98713e17adc46a506e25ab913ca7db0f8a8b79c
Stefan van Tilborg <stefan@xprtz,net>
2020-12-27 18:34:33 +0100

    added boterham

---
+++ boterham/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace boterham
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
