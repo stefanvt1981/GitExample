0416325d9d9fec48a62fe88aef494e69992ac338
Stefan van Tilborg <stefan@xprtz,net>
2020-12-27 11:58:28 +0100

    added worst

---
+++ worst/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace worst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
